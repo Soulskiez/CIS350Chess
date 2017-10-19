6a54723583f84b72d3b98558677433e804afc708
Soulskiez <tyler_solnikowski@aol.com>
2017-10-18 22:07:00 -0400

    adding queen and adjusted bishop

--- src/chess/Queen.java
+++ src/chess/Queen.java
@@ -1,9 +1,12 @@
 package chess;
 
-public class Bishop extends Piece {
-	private Player player;
+public class Queen extends Piece {
+private Player player;
+private Rook rook;
+private Bishop bishop;
+private Piece piece;
 	
-	protected Bishop(Player player){
+	protected Queen(Player player){
 		super(player);
 	}
 	@Override
@@ -13,12 +16,11 @@ public class Bishop extends Piece {
 
 	@Override
 	public String type() {
-		return "Bishop";
+		return "Queen";
 	}
 
 	@Override
 	public boolean isValidMove(Move move, Piece[][] board) {
-		
 		boolean valid = false;
 		int diffC = Math.abs(move.fromColumn - move.toColumn);
 		int diffR = Math.abs(move.fromRow - move.toRow);
@@ -37,12 +39,7 @@ public class Bishop extends Piece {
 						valid = true;
 					}
 					else if(board[j][i].player() != board[move.fromRow][move.fromColumn].player()){
-						if(board[j][i] == board[move.toRow][move.toColumn]){
-							valid = true;
-						}
-						else{
-							valid = false;
-						}
+						valid = true;
 						break;
 					}
 					else if(board[j][i].player() == board[move.fromRow][move.fromColumn].player()){
@@ -134,7 +131,110 @@ public class Bishop extends Piece {
 					j++;
 				}
 			}
-		}	
+			// Move up or down
+			if (move.fromColumn == move.toColumn) {
+				// Move up
+				if (move.fromRow > move.toRow) {
+					i = move.fromRow - 1;
+					while(i >= move.toRow) {
+						if (board[i][move.toColumn] == null)
+							valid = true;
+						else {
+							if (board[i][move.fromColumn].player() != board[move.fromRow][move.fromColumn].player()) {
+								if (i == move.toRow)
+									valid = true;
+								else {
+									valid = false;
+									break;
+								}
+							}
+							if (board[i][move.fromColumn].player() == board[move.fromRow][move.fromColumn].player()) {
+								valid = false;
+								break;
+							}
+						}
+						i--;
+					}
+				} // end of move up
+
+				// Move down
+				if (move.fromRow < move.toRow) {
+					i = move.fromRow + 1;
+					while(i <= move.toRow) {
+						if (board[i][move.toColumn] == null)
+							valid = true;
+						else {
+							if (board[i][move.fromColumn].player() != board[move.fromRow][move.fromColumn].player()) {
+								if (i == move.toRow)
+									valid = true;
+								else {
+									valid = false;
+									break;
+								}
+							}
+							if (board[i][move.fromColumn].player() == board[move.fromRow][move.fromColumn].player()) {
+								valid = false;
+								break;
+							}
+						}
+						i++;
+					}
+				} // end of move down
+			}
+			// Move left or right
+			if (move.fromRow == move.toRow) {
+				// Move left
+				if (move.fromColumn > move.toColumn) {
+					j = move.fromColumn - 1;
+					while(j >= move.toColumn) {
+						if (board[move.fromRow][j] == null)
+							valid = true;
+						else {
+							if (board[move.fromRow][j].player() != board[move.fromRow][move.fromColumn].player()) {
+								if (j == move.toColumn)
+									valid = true;
+								else {
+									valid = false;
+									break;
+								}
+							}
+							if (board[move.fromRow][j].player() == board[move.fromRow][move.fromColumn].player()) {
+								valid = false;
+								break;
+							}
+						}
+						j--;
+					}
+
+				} // end of move left
+
+				// Move right
+				if (move.fromColumn > move.toColumn) {
+					j = move.fromColumn + 1;
+					while(j <= move.toColumn) {
+						if (board[move.fromRow][j] == null)
+							valid = true;
+						else {
+							if (board[move.fromRow][j].player() != board[move.fromRow][move.fromColumn].player()) {
+								if (j == move.toColumn)
+									valid = true;
+								else {
+									valid = false;
+									break;
+								}
+							}
+							if (board[move.fromRow][j].player() == board[move.fromRow][move.fromColumn].player()) {
+								valid = false;
+								break;
+							}
+						}
+						j++;
+					}
+				} // end of move right
+			}
+			
+		}
+		
 		return valid;
 	}
 }
