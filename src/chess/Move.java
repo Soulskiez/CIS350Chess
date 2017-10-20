80a48f7344283a5ece506627c84ce26c0b286f13
kaydavis314 <kaydavis314@gmail.com>
2017-10-20 08:45:57 -0400

    Update Move.java

--- src/chess/Move.java
+++ src/chess/Move.java
@@ -1,5 +1,13 @@
 package chess;
 
+/**
+ * Constructor for Move class
+ *
+ * @author Tyler Solnikowski, Kanoa Ellis,  Kayla Davis
+ * @version October 19th, 2017
+ *
+ */
+
 public class Move {
 	public int fromRow;
 	public int toRow;
@@ -8,6 +16,15 @@ public class Move {
 	
 	public Move(){
 	}
+	
+	/**
+	 * Takes provided location and stores it in order to later move the piece to that location.
+	 * 
+	 * @param fromRow row where the piece being moved is located
+	 * @param toRow row where the piece being moved is going
+	 * @param fromCol column where the piece being moved is located
+	 * @param toCol column where the piece being moved is going
+	 */
 	public Move(int fromRow,int toRow,int fromCol, int toCol){
 		this.fromRow = fromRow;
 		this.toRow = toRow;
